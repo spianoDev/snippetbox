e42f4678c186253f6922823fc9b2a11c7d97f185
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2022-01-03 12:58:22 -0600

    added two more functions for additional web pages

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from SpianoDev's Snippetbox"))
 }
 
+// Below is a showSnippet handler function to display a specific showSnippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Here is that snippet you asked for..."))
+}
+
+// Below is a createSnippet handler function that will make a new snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Creating a new snippet right away!"))
+}
+
 // Below is the main function that
 // First initializes a new servemux and registers the home function as "/" in the URL pattern
 // Second starts a new web server by passing in the TCP network address and the new servemux
@@ -18,6 +28,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet", showSnippet)
+    mux.HandleFunc("/snippet/create", createSnippet)
 
     log.Println("Serving up GO on :4000")
     err := http.ListenAndServe(":4000", mux)
