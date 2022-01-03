844bf7640977a30906927db3d8d8d2c01b45802a
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2022-01-03 15:16:42 -0600

    added stylesheet

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,11 @@ func main() {
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
 
+    // Serve the static files with relative path
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    // register the file server but strip /static prefix before the request
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     log.Println("Serving up GO on :4000")
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
