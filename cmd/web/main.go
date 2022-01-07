409e7b2b587cd6679a384bf6486461dc2c59e275
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2022-01-07 09:46:44 -0600

    added notes about setting addr as env variable

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,3 +27,9 @@ func main() {
     err := http.ListenAndServe(*addr, mux)
     log.Fatal(err)
 }
+
+// You can store the config settings in env variables by using the below function
+// addr := os.Getenv("SNIPPETBOX_ADDR")
+// Then, when starting the application, you can run the following bash commands to set a preferred address port
+// export SNIPPETBOX_ADDR=":4444" (replace the string value with preferred port)
+// go run ./cmd/web -addr=$SNIPPETBOX_ADDR
