20b8e1bc1094c0eb65050da040a22502e7acedc1
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2022-01-14 13:08:21 -0600

    adding security headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
     mux := http.NewServeMux()
     mux.HandleFunc("/", app.home)
     mux.HandleFunc("/snippet", app.showSnippet)
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
     // register the file server but strip /static prefix before the request
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    return mux
+    return secureHeaders(mux)
 }
